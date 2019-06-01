79047a8ff1e4b2b8aced08adf4420ee459adf7b0
AaskaShah <aaskashah1996@gmail.com>
2019-06-02 04:38:18 +0530

    Pay to join contest added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, Query, EventEmitter, Output } from '@angular/core';
 import * as Chartist from 'chartist';
+import Matic from 'maticjs'
+import { config }from './config';
 import { Apollo } from 'apollo-angular';
 import * as Queries from '../query';
 import { DataService } from '../data.service';
+//declare const payToJoin: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -13,10 +16,63 @@ export class DashboardComponent implements OnInit {
 
   contests = []
 
-  constructor(private apollo: Apollo, private data: DataService) { }
+  constructor(private apollo: Apollo, private data: DataService) {
+      this.initMatic();
+   }
   
+
+privatekeys = {
+  '0x5d659380a95f32d6960b73C803B962d204c9584f': '0x13a41d93a304a12e0df873061c4fa68ab4dbe5753a4bcb9eb223045b5b81457d',
+  '0x7C250149936Cfd91f6145963287A2F388DF9bA28': '0xd49fd22d55bac06e202ba752090746afecbc32e8d5a39beef726edb216f15ddc',
+  '0xc27daa57faBC85884E63CC9A6EFf6591b39994aA': '0x1293e70e1d4457d4714b79995679d5448fd0f3c29eed8bd8dfcf96e368559433'
+};
+
+
+  matic = null
+
+  // Create sdk instance
+  initMatic() {
+    if (!this.matic) {
+      this.matic = new Matic({
+        maticProvider: config.MATIC_PROVIDER,
+        parentProvider: config.PARENT_PROVIDER,
+        rootChainAddress: config.ROOTCHAIN_ADDRESS,
+        maticWethAddress: config.MATICWETH_ADDRESS,
+        syncerUrl: config.SYNCER_URL,
+        watcherUrl: config.WATCHER_URL,
+        withdrawManagerAddress: config.WITHDRAWMANAGER_ADDRESS,  
+      })
+
+      //this.matic.wallet = '<private-key>' // your private key
+    }
+  }
+
+  payToJoin(account, amount) {
+    console.log(this.matic);
+    const token = config.MATIC_TEST_TOKEN;
+    const owner = config.OWNER_ADDRESS;
+
+    this.matic.wallet = this.privatekeys[account]
+
+    console.log("Token : " + token);
+    console.log("Account : " + account);
+    console.log("To receipient owner: " + owner + " for key: " + this.privatekeys[account]);
+
+    this.matic.transferTokens(token, owner, amount, {
+      from: account,
+      onTransactionHash: hash => {
+        // action on Transaction success
+        // eslint-disable-next-line
+        console.log(hash);
+      },
+    })
+  }
+
+
   enterContest(id: any){
     console.log(id)
+    //payToJoin("0x7C250149936Cfd91f6145963287A2F388DF9bA28", 1000000000000000000);
+    this.payToJoin("0x7C250149936Cfd91f6145963287A2F388DF9bA28", '1000000000000000000');
     this.data.changeMessage(id);    
   
   };
